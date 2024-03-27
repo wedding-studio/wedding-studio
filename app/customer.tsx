94562f437a84703ece0831974b51ea5fd8bb389f
Nguyễn Quang Minh (NQM) <nguyenquangminh570@gmaiil.com>
2024-03-27 12:21:31 +0700

    Add customer, showInfoAccount components

---
+++ app/customer.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const customer = () => {
+  return (
+    <View>
+      <Text>customer</Text>
+    </View>
+  );
+};
+
+export default customer;
+
+const styles = StyleSheet.create({});
---
+++ app/showInfoAccount.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const showInfoAccount = () => {
+  return (
+    <View>
+      <Text>showInfoAccount</Text>
+    </View>
+  );
+};
+
+export default showInfoAccount;
+
+const styles = StyleSheet.create({});
